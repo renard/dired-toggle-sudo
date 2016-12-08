9e8b3dd7fb99dacf3b639c4dfac467b5aa5d9cf3
Jonathan Kotta <jpkotta@packetdigital.com>
2016-12-08 12:34:47 -0600

    fix typo

--- dired-toggle-sudo.el
+++ dired-toggle-sudo.el
@@ -14,7 +14,7 @@
 ;;
 ;; Allow to switch from current user to sudo when browsind `dired' buffers.
 ;;
-;; To activate and swit with "C-c C-s" just put in your .emacs:
+;; To activate and switch with "C-c C-s" just put in your .emacs:
 ;;
 ;; (require 'dired-toggle-sudo)
 ;; (define-key dired-mode-map (kbd "C-c C-s") 'dired-toggle-sudo)
@@ -23,8 +23,6 @@
 ;;     ;; Allow to use: /sudo:user@host:/path/to/file
 ;;     (add-to-list 'tramp-default-proxies-alist
 ;;		  '(".*" "\\`.+\\'" "/ssh:%h:"))))
-
-
 
 ;;; Code:
 
